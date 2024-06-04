25d3022d4ec6e52b0d24df8b6dfc97ca275ad2e3
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2024-06-03 23:58:55 -0400

    Button update

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -11,7 +11,7 @@ import {HiOutlineExclamationCircle} from 'react-icons/hi';
 
 
 export default function DashProfile() {
-    const {currentUser, error}= useSelector((state) => state.user);
+    const {currentUser, error, loading}= useSelector((state) => state.user);
     const [imageFile, setImageFile]= useState(null);
     const [imageFileUrl, setImageFileUrl]=useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -31,7 +31,7 @@ export default function DashProfile() {
             setImageFile(file);
             setImageFileUrl(URL.createObjectURL(file));
         }
-    }
+    };
     useEffect(()=> {
         if(imageFile){
             uploadImage();
@@ -177,8 +177,9 @@ export default function DashProfile() {
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange}/>
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}/>
         <TextInput type='password' id='password' placeholder='password' onChange={handleChange}/>
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-            Update
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading}>
+
+            {loading? 'Loading...' : 'Update'}
         </Button>
         {
             currentUser.isAdmin && (
