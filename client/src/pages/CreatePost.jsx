4758f6e5fce45f32efde7c20b4e9afd954df0473
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2024-06-09 23:55:14 -0400

    handle submit function

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -13,6 +13,7 @@ export default function CreatePost() {
   const [imageUploadProgress, setImageUploadProgress]= useState(null);
   const [imageUploadError, setImageUploadError]= useState(null);
   const [formData, setFormData]= useState({});
+  const [publishError, setPublishError]= useState(null);
   const handleUploadImage= useCallback(async ()=> {
     try {
       if(!file){
@@ -50,18 +51,40 @@ export default function CreatePost() {
       console.log(error);
     }
   });
+  const handleSubmit= async(e) => {
+    e.preventDefault();
+    try {
+      const res= await fetch('api/post/create', {
+        method: 'POST',
+        headers: {
+          'content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data= await res.json();
+      if(!res.ok){
+        setPublishError(data.message)
+        return
+      }
+      if(res.ok){
+        setPublishError(null)
+      }
+    } catch (error) {
+      setPublishError('Something went wrong');
+    }
+  }
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className="text-center text-3xl my-7 font-semibold">Create a Post</h1>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
 
             <div className="flex flex-col gap-4 sm:flex-row justify-between">
               <TextInput type='text' placeholder='Title' required id='title' className='flex-1' onChange={(e)=> setFormData({...formData, title: e.target.value})}/>
               <Select onChange={(e)=> setFormData({...formData, category: e.target.value})}>
                 <option value="uncategorized">Select a Category</option>
-                <option value="uncategorized">JavaScript</option>
-                <option value="uncategorized">React.js</option>
-                <option value="uncategorized">Next.js</option>
+                <option value="JavaScript">JavaScript</option>
+                <option value="React.js">React.js</option>
+                <option value="Next.js">Next.js</option>
               </Select>               
             </div>
 
