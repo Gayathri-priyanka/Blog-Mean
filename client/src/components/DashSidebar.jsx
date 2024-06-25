c5110bdb042e25a956508de608d697c6b114847f
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2024-06-24 22:48:40 -0400

    dash side bar and copmments page

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from 'flowbite-react'
 import React from 'react';
-import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
+import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
 import  { useEffect, useState } from 'react'
 import {useLocation} from 'react-router-dom';
 import {Link} from 'react-router-dom';
@@ -58,12 +58,23 @@ export default function DashSidebar() {
               </Link>
             )}
 
+            
+
             {currentUser.isAdmin && (
-              <Link to='/dashboard?tab=users'>
+              <>
+
+            <Link to='/dashboard?tab=users'>
               <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div'>
                 Users
               </Sidebar.Item>
               </Link>
+
+              <Link to='/dashboard?tab=comments'>
+              <Sidebar.Item active={tab === 'comments'} icon={HiAnnotation} as='div'>
+                Comments
+              </Sidebar.Item>
+              </Link>
+              </>
             )}
             
 
