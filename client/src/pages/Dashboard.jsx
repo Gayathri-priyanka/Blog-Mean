ca3ee261750380c2f1b834391d2a4940c342e58b
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2024-06-26 02:54:30 -0400

    Add dashboard overview to the dashboard page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashBoardComp from '../components/DashBoardComp';
 
 
 export default function Dashboard() {
@@ -34,6 +35,9 @@ export default function Dashboard() {
 
       {/* comments, ... */}
       {tab === 'comments' && <DashComments/>}
+
+      {/* dahsboard comp, ... */}
+      {tab === 'dash' && <DashBoardComp/>}
     </div>
   )
 }
